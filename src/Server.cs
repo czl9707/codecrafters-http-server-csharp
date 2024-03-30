6bb631e9331b14848bc632f0755242b9ddb80ebb
chenZL <czl970721@gmail.com>
2024-03-30 10:18:44 -0400

    send empty response

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,33 @@
+using System.Globalization;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
-server.Start();
-server.AcceptSocket(); // wait for client
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 4221);
+        server.Start();
+
+        Console.WriteLine("server start");
+
+        while (true)
+        {
+            using var socket = server.AcceptSocket();
+            // using var stream = new NetworkStream(socket);
+            // using var writer = new StreamWriter(stream);
+            // using var reader = new StreamReader(stream);
+            Console.WriteLine("socket");
+
+            var OKResponse = "HTTP/1.1 200 OK\r\n\r\n";
+            var OKResponseBytes = Encoding.UTF8.GetBytes(OKResponse);
+            // await writer.WriteAsync(OKResponse);
+            // await writer.FlushAsync();
+            await socket.SendAsync(OKResponseBytes, SocketFlags.None);
+
+            Console.WriteLine("socket close");
+        }
+    }
+}
