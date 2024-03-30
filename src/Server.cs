beb5500d0ca7be3137916716c6f6a8fb2d8656ce
chenZL <czl970721@gmail.com>
2024-03-30 19:31:54 -0400

    task schedule

--- src/Server.cs
+++ src/Server.cs
@@ -15,58 +15,64 @@ internal class Program
 
         while (true)
         {
-            using var socket = server.AcceptSocket();
+            var socket = server.AcceptSocket();
+            var _ = Task.Run(() => RequestHandler(socket));
+        }
+    }
+
+    private static async Task RequestHandler(Socket socket)
+    {
+        using (socket)
+        {
             using var stream = new NetworkStream(socket);
             using var reader = new StreamReader(stream);
             using var writer = new StreamWriter(stream);
 
-            await RequestHandler(reader, writer);
-        }
-    }
+            while (socket.Connected)
+            {
+                var buffer = new char[1024];
+                int bytesRead = await reader.ReadAsync(buffer, 0, buffer.Length);
+                if (bytesRead == 0)
+                {
+                    return;
+                }
 
-    private static async Task RequestHandler(StreamReader reader, StreamWriter writer)
-    {
-        var buffer = new char[1024];
-        int bytesRead = await reader.ReadAsync(buffer, 0, buffer.Length);
-        if (bytesRead == 0)
-        {
-            return;
-        }
+                var requestString = new StringBuilder().Append(buffer, 0, bytesRead).ToString();
 
-        var requestString = new StringBuilder().Append(buffer, 0, bytesRead).ToString();
+                var request = HTTPRequest.FromString(requestString);
+                HTTPResponse response;
 
-        var request = HTTPRequest.FromString(requestString);
-        HTTPResponse response;
+                if (request.Path == "/")
+                {
+                    response = new OK();
+                }
+                else if (request.Path.StartsWith("/echo"))
+                {
+                    var content = request.Path.Substring("/echo/".Length);
+                    response = new OK();
+                    response.Content = content;
+                    response.Headers.Add("Content-Length", content.Length.ToString());
+                    response.Headers.Add("Content-Type", "text/plain");
+                }
+                else if (request.Path.StartsWith("/user-agent"))
+                {
+                    request.Headers.TryGetValue("user-agent", out var content);
+                    content = content ?? string.Empty;
 
-        if (request.Path == "/")
-        {
-            response = new OK();
-        }
-        else if (request.Path.StartsWith("/echo"))
-        {
-            var content = request.Path.Substring("/echo/".Length);
-            response = new OK();
-            response.Content = content;
-            response.Headers.Add("Content-Length", content.Length.ToString());
-            response.Headers.Add("Content-Type", "text/plain");
-        }
-        else if (request.Path.StartsWith("/user-agent"))
-        {
-            request.Headers.TryGetValue("user-agent", out var content);
-            content = content ?? string.Empty;
+                    response = new OK();
+                    response.Content = content;
+                    response.Headers.Add("Content-Length", content.Length.ToString());
+                    response.Headers.Add("Content-Type", "text/plain");
+                }
+                else
+                {
+                    response = new NotFound();
+                }
 
-            response = new OK();
-            response.Content = content;
-            response.Headers.Add("Content-Length", content.Length.ToString());
-            response.Headers.Add("Content-Type", "text/plain");
+                // Console.WriteLine(response.AsString());
+                await writer.WriteAsync(response.AsString());
+                await writer.FlushAsync();
+            }
         }
-        else
-        {
-            response = new NotFound();
-        }
-
-        // Console.WriteLine(response.AsString());
-        await writer.WriteAsync(response.AsString());
-        await writer.FlushAsync();
     }
 }
