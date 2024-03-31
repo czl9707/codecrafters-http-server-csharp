826182347bfffcbb50450a7993e2fbcc6b52dd2a
chenZL <czl970721@gmail.com>
2024-03-31 17:56:18 -0400

    file load

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,8 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.IO;
+using System.CommandLine;
 using MyHttpServer.Model.Request;
 using MyHttpServer.Model.Response;
 
@@ -8,8 +10,22 @@ namespace MyHttpServer;
 
 internal class Program
 {
+    internal static string? Directory { get; set; }
     internal static async Task Main(string[] args)
     {
+        var rootCommand = new RootCommand("MyHttpServer");
+        var directoryOption = new Option<string?>("--directory",
+            description: "An option declare directory the server hosted in.",
+            getDefaultValue: () => null);
+        rootCommand.AddOption(directoryOption);
+
+        rootCommand.SetHandler((context) =>
+        {
+            var directory = context.ParseResult.GetValueForOption(directoryOption);
+            Program.Directory = directory;
+        });
+        rootCommand.Invoke(args);
+
         TcpListener server = new TcpListener(IPAddress.Any, 4221);
         server.Start();
 
@@ -17,6 +33,26 @@ internal class Program
         {
             var socket = server.AcceptSocket();
             var _ = Task.Run(() => RequestHandler(socket));
+        }
+    }
+
+    private static async Task<string> ReadStringAsync(StreamReader reader)
+    {
+        try
+        {
+            var buffer = new char[reader.BaseStream.CanSeek ? reader.BaseStream.Length : 1024];
+            int bytesRead = await reader.ReadAsync(buffer, 0, buffer.Length);
+            if (bytesRead == 0)
+            {
+                return String.Empty;
+            }
+
+            return new StringBuilder().Append(buffer).ToString();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
         }
     }
 
@@ -30,15 +66,7 @@ internal class Program
 
             while (socket.Connected)
             {
-                var buffer = new char[1024];
-                int bytesRead = await reader.ReadAsync(buffer, 0, buffer.Length);
-                if (bytesRead == 0)
-                {
-                    return;
-                }
-
-                var requestString = new StringBuilder().Append(buffer, 0, bytesRead).ToString();
-
+                var requestString = await ReadStringAsync(reader);
                 var request = HTTPRequest.FromString(requestString);
                 HTTPResponse response;
 
@@ -63,6 +91,30 @@ internal class Program
                     response.Content = content;
                     response.Headers.Add("Content-Length", content.Length.ToString());
                     response.Headers.Add("Content-Type", "text/plain");
+                }
+                else if (request.Path.StartsWith("/files"))
+                {
+                    var filename = request.Path.Substring("/files/".Length);
+                    if (Program.Directory is null)
+                    {
+                        response = new NotFound();
+                    }
+                    else if (!File.Exists(Path.Combine(Program.Directory, filename)))
+                    {
+                        response = new NotFound();
+                    }
+                    else
+                    {
+                        var fullPath = Path.Combine(Program.Directory, filename);
+                        var fileContent = await ReadStringAsync(
+                            new StreamReader(File.Open(fullPath, FileMode.Open))
+                        );
+
+                        response = new OK();
+                        response.Content = fileContent;
+                        response.Headers.Add("Content-Length", fileContent.Length.ToString());
+                        response.Headers.Add("Content-Type", "application/octet-stream");
+                    }
                 }
                 else
                 {
